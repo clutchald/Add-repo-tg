954a4b22744688080c072c688ceef7b00179f5f6
clutchald <legitclutch94@gmail.com>
2026-06-13 15:04:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID = 13691
-API_HASH = "2a31b117896c5c7da27c725aa602b8"
-BOT_TOKEN = "8812613640:AAEa3mbFCdMiNI4AyTtVAD8jJXM15ciTw"
-MONGO_URI = "mongodb+srv://RAJ:RAJ@outl.itqomxm.mongodb.net/?appName=outlook"
+API_ID = 13691707
+API_HASH = "2a31b117896c5c7da27c74025aa602b8"
+BOT_TOKEN = "8812613640:AAG5i-J1k1I_AtA-RzWl8_WO9c-gjMQ29FQ"
+MONGO_URI = "mongodb+srv://RAJ:RAJ@outlook.itqomxm.mongodb.net/?appName=outlook"
 
 SUDO_USERS = [
     5903688119,
