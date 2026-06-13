337ab7f63c78666d3f19d69733eba6550b21abf2
clutchald <legitclutch94@gmail.com>
2026-06-13 13:39:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 API_ID = 13691707
 API_HASH = "2a31b117896c5c7da27c74025aa602b8"
-BOT_TOKEN = "8812613640:AAFlLBcd4qW09PHvDQzbxNzDjEev3BgpVCw"
+BOT_TOKEN = "8812613640:AAEa3mbFC8kdMiNI4AyTtVAD8jJXM15ciTw"
 MONGO_URI = "mongodb+srv://RAJ:RAJ@outlook.itqomxm.mongodb.net/?appName=outlook"
 
 SUDO_USERS = [
