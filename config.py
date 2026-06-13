9a17c8aedf1e2a6d5120ca587d530e1cf846e547
clutchald <legitclutch94@gmail.com>
2026-06-13 12:56:11 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+API_ID = 13691707
+API_HASH = "2a31b117896c5c7da27c74025aa602b8"
+BOT_TOKEN = "8812613640:AAFlLBcd4qW09PHvDQzbxNzDjEev3BgpVCw"
+MONGO_URI = "mongodb+srv://RAJ:RAJ@outlook.itqomxm.mongodb.net/?appName=outlook"
+
+SUDO_USERS = [
+    5903688119,
+    8002345109
+]
